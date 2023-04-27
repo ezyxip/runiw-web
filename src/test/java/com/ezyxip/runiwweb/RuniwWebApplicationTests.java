8a1f73328fb6f287aca3c66854c90935080ef1c5
Antey <anzej2003@gmail.com>
2023-04-27 23:58:13 +0600

    Первый коммит - начало

---
+++ src/test/java/com/ezyxip/runiwweb/RuniwWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezyxip.runiwweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuniwWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
